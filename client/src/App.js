5f67783a86fc719b7aa2ab3c675615715b401d1c
Sachinsr56 <sachinkumarhas@gmail.com>
2023-02-10 22:25:42 +0530

    Deploy

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
 import Home from './components/home';
 import Chats from './components/chats';
 import ChatProvider from './Context/ChatProvider';
+import Error from './Error';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chats" element={<Chats />} />
+        <Route element={<Error />} />
       </Routes>
       </ChatProvider>
     </Router>
